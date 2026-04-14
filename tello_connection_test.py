ae6c840efa8bb23309a33c04b16e173f6414a18e
Eduardo Morales <e.moralesguzman21@gmail.com>
2026-04-14 10:16:44 -0600

    Added a test connection to Tello Drone

--- tello_connection_test.py
+++ tello_connection_test.py
@@ -1,1 +1,16 @@
-print ("Hello world")
+from djitellopy import Tello
+
+tello = Tello()
+
+print("Connecting to Tello...")
+tello.connect()
+print("Battery level:", tello.get_battery())
+
+print("Taking off...")
+tello.takeoff()
+
+tello.move_up(50)
+
+print("Landing...")
+tello.land()
+tello.end()
